abbaee606bb84a2141f1dd291ec156be3138b000
Maloochow <luyao.mly@gmail.com>
2020-09-30 22:41:42 -0400

    Done.

--- src/containers/GifListContainer.js
+++ src/containers/GifListContainer.js
@@ -1,40 +1,41 @@
-import React, { Component } from 'react'
-import GifList from '../components/GifList'
-import GifSearch from '../components/GifSearch'
+import React, { Component } from "react";
+import GifList from "../components/GifList";
+import GifSearch from "../components/GifSearch";
 
 class GifListContainer extends Component {
+  state = {
+    gifs: [],
+  };
 
-    state = {
-        gifs: []
-    }
+  componentDidMount() {
+    this.fetchGifs();
+  }
 
-    componentDidMount() {
-        this.fetchGifs()
-    }
+  // componentDidUpdate() {
 
-    // componentDidUpdate() {
+  //     console.log("Good job your search worked!")
+  // }
 
-    //     console.log("Good job your search worked!")
-    // }
+  fetchGifs = (term = "dolphins") => {
+    fetch(
+      `https://api.giphy.com/v1/gifs/search?api_key=PUZ1GjZ90MZDGClkzhhbhP8P7DLHsIKz&q=${term}&limit=25&offset=0&rating=g&lang=en`
+    )
+      .then((resp) => resp.json())
+      .then((data) => this.setState({ gifs: data.data.slice(0, 3) }));
+  };
 
-    fetchGifs = (term = "dolphins") => {
-        fetch(`https://api.giphy.com/v1/gifs/search?q=${term}&api_key=dc6zaTOxFJmzC&rating=g&limit=10`)
-            .then(resp => resp.json())
-            .then(data => this.setState({ gifs: data.data }))
-    }
+  submitHandler = (searchTerm) => {
+    this.fetchGifs(searchTerm);
+  };
 
-    submitHandler = (searchTerm) => {
-        this.fetchGifs(searchTerm)
-    }
-
-    render() {
-        return (
-            <React.Fragment>
-                <GifSearch submitHandler={this.submitHandler} />
-                <GifList gifs={this.state.gifs} />
-            </React.Fragment>
-        )
-    }
+  render() {
+    return (
+      <React.Fragment>
+        <GifSearch submitHandler={this.submitHandler} />
+        <GifList gifs={this.state.gifs} />
+      </React.Fragment>
+    );
+  }
 }
 
-export default GifListContainer
+export default GifListContainer;
